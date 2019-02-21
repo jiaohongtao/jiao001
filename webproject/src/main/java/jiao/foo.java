e7f1457b7861e1715f936456c7a16c225f719fbe
jiaoht <18332120276@163.com>
2019-02-21 14:30:02 +0800

    modified:   webproject/package/bkjava.png  deleted:    webproject/package/hh.png new file:   webproject/src/main/java/jiao/foo.java

---
+++ webproject/src/main/java/jiao/foo.java
@@ -0,0 +1,13 @@
+package jiao;
+
+public class foo
+{
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 5;
+		String s1 = "抓到是否";
+		String s2 = s1;
+		System.out.println(a == b);
+		System.out.println(s1 == s2);
+	}
+}
